2785235f414e7445adb4061a89999a6477ed2eb2
secInfoHgun <SECINFO@hgunatal.com.br>
2022-03-14 10:57:13 -0300

    Initial commit

---
+++ src/test/java/com/hgun/sti/BaseTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgun.sti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
